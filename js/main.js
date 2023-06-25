eac0f67c6f7c5089e6236aaee2cdef487ac821d6
ls_des <kyh9827@lscompany.co.kr>
2023-06-26 00:35:17 +0900

    230626

---
+++ js/main.js
@@ -0,0 +1,127 @@
+import store from '../src/storeDB.js'
+
+new Swiper('#info .swiper', {
+  autoplay: true, 
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '#info .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '#info .swiper-button-prev',
+    nextEl: '#info .swiper-button-next'
+  }
+})
+
+new Swiper('.park .swiper', {
+  // autoplay: true, 
+  loop: true,
+  effect : 'fade',
+  speed: 500,
+  slidesPerView: 1,
+  pagination: {
+    el: '.park .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.park .swiper-button-prev',
+    nextEl: '.park .swiper-button-next'
+  }
+})
+
+new Swiper('.sports .swiper', {
+  // autoplay: true, 
+  loop: true,
+  effect : 'fade',
+  speed: 500,
+  slidesPerView: 1,
+  pagination: {
+    el: '.sports .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.sports .swiper-button-prev',
+    nextEl: '.sports .swiper-button-next'
+  }
+})
+
+new Swiper('.zoo .swiper', {
+  // autoplay: true, 
+  loop: true,
+  effect : 'fade',
+  speed: 500,
+  slidesPerView: 1,
+  pagination: {
+    el: '.zoo .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.zoo .swiper-button-prev',
+    nextEl: '.zoo .swiper-button-next'
+  }
+})
+
+new Swiper('.subVisual03 .swiper', {
+  // autoplay: true, 
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.subVisual03 .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.subVisual03 .swiper-button-prev',
+    nextEl: '.subVisual03 .swiper-button-next'
+  }
+})
+
+new Swiper('.subVisual04 .swiper', {
+  // autoplay: true, 
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.subVisual04 .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.subVisual04 .swiper-button-prev',
+    nextEl: '.subVisual04 .swiper-button-next'
+  }
+})
+
+const itemsEl = document.querySelector('#subVisual03 .swiper-wrapper')
+store.forEach(function(store){
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('swiper-slide')
+  itemEl.innerHTML = /* html */ `
+    <div class="swiper-slide">
+      <div class="tit-wrap">
+        <div class="tit-mask diamond"><span class="tit">경주 2호점</span></div>
+      </div>
+      <div class="info">
+          <ul class="txt-wrap">
+            <li class="name ex-bold">${store.name}</li>
+            <li>
+              <span class="bold">매장주소</span>
+              <span>${store.add}</span>
+            </li>
+            <li>
+              <span class="bold">전화번호</span>
+              <span>${store.tel}</span>
+            </li>
+            <li>
+              <span class="bold">영업시간</span>
+              <span>${store.time}</span>
+            </li>
+          </ul>
+          <div class="button-wrap">
+            <button class="btn btn--blue">지도</button>
+            <button class="btn">유선문의</button>
+          </div>
+      </div>
+    </div>
+  `
+
+  itemsEl.append(itemEl)
+})
